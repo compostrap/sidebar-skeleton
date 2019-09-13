b580112f0860a19cd58d4d96045960013f14186d
accgit <zdenek.papucik@gmail.com>
2019-09-13 12:30:14 +0200

    Update

--- dist/js/sidebar.js
+++ dist/js/sidebar.js
@@ -5,6 +5,17 @@
  */
 (function($) {
 
+  // see if navbar is used
+  $(document).ready(function () {
+
+    var el = $(this);
+    var navbar  = '.navbar';
+    var finder  = el.find('.wrapper');
+
+    el.find(navbar).length > 0 ? finder.addClass('wrapper--navbar-used') : null;
+    el.find(navbar + '.fixed-top').length > 0 ? finder.addClass('wrapper--navbar-fixed') : null;
+  });
+
   // toggle sidebar
   $('#sidebar').on('click', function() {
     $('#navigation').toggleClass('sidebar--toggle');
