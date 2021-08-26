d8eedec7371e1b72fce44addd31d28a3a9bf5a7a
accgit <zdenek.papucik@gmail.com>
2021-08-26 09:11:50 +0200

    Update

--- dist/js/sidebar.js
+++ dist/js/sidebar.js
@@ -3,7 +3,9 @@
  * Copyright Zdeněk Papučík
  * MIT License
  */
-document.getElementById('sidebar').addEventListener('click', () => {
-  const element = document.getElementById('navigation');
-  element.classList.toggle('sidebar-toggle');
+document.addEventListener('DOMContentLoaded', () => {
+  document.getElementById('sidebar').addEventListener('click', () => {
+    const element = document.getElementById('navigation');
+    element.classList.toggle('sidebar-toggle');
+  });
 });
