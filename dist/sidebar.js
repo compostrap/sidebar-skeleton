76addc0b0ea8c4509c3cc4c646c9147be8ed159e
accgit <zdenek.papucik@gmail.com>
2025-12-31 10:45:02 +0100

    dev

---
+++ dist/sidebar.js
@@ -0,0 +1,1 @@
+document.addEventListener("DOMContentLoaded",()=>{document.getElementById("sidebar").addEventListener("click",()=>{document.getElementById("navigation").classList.toggle("sidebar-toggle")})});
