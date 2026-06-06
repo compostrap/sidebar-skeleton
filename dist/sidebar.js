dd057d1064c2c420563c714c3c26c522a09c8072
accgit <zdenek.papucik@gmail.com>
2026-06-06 06:41:20 +0200

    Update

--- dist/sidebar.js
+++ dist/sidebar.js
@@ -1,11 +1,19 @@
-class n {
+class d {
   static init() {
-    const t = document.getElementById("sidebar"), e = document.getElementById("navigation");
-    t && e && t.addEventListener("click", () => {
-      e.classList.toggle("sidebar-toggle");
-    });
+    const s = document.getElementById("sidebar"), e = document.getElementById("navigation"), t = document.createElement("div");
+    if (t.className = "sidebar-backdrop", s && e) {
+      e.after(t);
+      const a = () => {
+        e.classList.remove("sidebar-toggle"), t.classList.remove("show");
+      }, i = window.matchMedia("(min-width: 768px)");
+      s.addEventListener("click", () => {
+        e.classList.toggle("sidebar-toggle"), t.classList.toggle("show", e.classList.contains("sidebar-toggle"));
+      }), t.addEventListener("click", a), i.addEventListener("change", (c) => {
+        c.matches && a();
+      });
+    }
   }
 }
 export {
-  n as Sidebar
+  d as Sidebar
 };
